e45bf65b3be182547e53bd949022989c42ef0531
Andriy Massimilla <andriymassimilla@gmail.com>
2016-08-25 13:34:22 -0400

    Cleaned up GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,4 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 	public const string VERSION = "0.2.2";
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
 }
