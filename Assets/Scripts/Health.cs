c1c6ac6259b5e0e7737eb9d525dad61095890b55
Andriy Massimilla <andriymassimilla@gmail.com>
2016-08-27 16:52:44 -0400

    Rename PlayerLogic to Health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+	public AudioClip DamageSound;
+	public int health = 100;
+
+	// Update is called once per frame
+	void Update() {
+		if (transform.position.y < -100) {
+			Die();
+		}
+
+		if (health <= 0) {
+			Die();
+		}
+	}
+
+	[PunRPC]
+	void TakeDamage(int amount) {
+		health -= amount;
+
+		GetComponent<AudioSource>().clip = DamageSound;
+		GetComponent<AudioSource>().Play();
+	}
+
+	void Die() {
+		// Check if object killed is instantiated on network
+		if (GetComponent<PhotonView>().instantiationId == 0) {
+			Destroy(gameObject);
+		} else {
+			if (GetComponent<PhotonView>().isMine) {
+				if (gameObject.tag == "Player") {
+					GameManager.inGame = false;
+				}
+
+				PhotonNetwork.Destroy(gameObject);
+			}
+		}
+	}
+}
